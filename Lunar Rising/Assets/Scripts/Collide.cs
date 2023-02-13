aa701bdaf2947daaf5cca0fb5d047695497c5f41
Zack C <Zack.Christenson@comcast.net>
2023-02-13 16:59:53 -0500

    Added a simple collision script and test
    Added a collision script that prints to console upon colliding with an object with a specific tag

---
+++ Lunar Rising/Assets/Scripts/Collide.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided with something");
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Collided with obstacle");
+        }
+    }
+}
