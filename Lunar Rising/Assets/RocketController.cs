8f20e681151d9cca6ea14eb13e644eadd6f834d3
Wyatt Davison <wsdavison04@gmail.com>
2023-02-20 17:10:51 -0500

    Movement

---
+++ Lunar Rising/Assets/RocketController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    [SerializeField] float thrusterForce = 10f;
+    [SerializeField] float tiltingForce = 10f;
+
+    bool thrust = false;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float tilt = Input.GetAxis("Horizontal");
+        thrust = Input.GetKey(KeyCode.W);
+
+        if(!Mathf.Approximately(tilt, 0f))
+        {
+            rb.freezeRotation = true;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (new Vector3(0f, 0f, (tilt * tiltingForce * Time.deltaTime))));
+        }
+
+        rb.freezeRotation = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust)
+        {
+            rb.AddRelativeForce(Vector3.up * thrusterForce * Time.deltaTime);
+        }
+    }
+}
