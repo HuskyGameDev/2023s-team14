0fdc1b185bf64684f7431e4a61344bbf22ab71f5
imnotbrendandud <81782166+imnotbrendandud@users.noreply.github.com>
2023-04-08 17:13:08 -0400

    Sky Background & platforms

--- Lunar Rising/Assets/Scripts/RocketController.cs
+++ Lunar Rising/Assets/Scripts/RocketController.cs
@@ -7,9 +7,19 @@ public class RocketController : MonoBehaviour
     [SerializeField] float thrusterForce = 10f;
     [SerializeField] float tiltingForce = 10f;
 
+    bool isGrounded = false;
+    public Transform GroundCheck;
+    public LayerMask groundLayer;
+
     bool thrust = false;
+    [SerializeField] float boostAmount = 100f;
+    [SerializeField] float rechargeInterval = 1f;
+    float time = 0f;
+    float delayAmount = 2f;
 
     Rigidbody2D rb;
+
+    [SerializeField] ParticleSystem thrustParticles;
 
     private void Awake()
     {
@@ -18,6 +28,9 @@ public class RocketController : MonoBehaviour
 
     private void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.15f, groundLayer);
+        Debug.Log(isGrounded);
+
         float tilt = Input.GetAxis("Horizontal");
         thrust = Input.GetKey(KeyCode.W);
 
@@ -32,14 +45,32 @@ public class RocketController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (thrust)
+        if (thrust && boostAmount > 0f)
         {
+            thrustParticles.Play();
             FindObjectOfType<AudioManager>().Play("rocket_thrust_sound");
             rb.AddRelativeForce(Vector3.up * thrusterForce * Time.deltaTime);
+            boostAmount = boostAmount - rechargeInterval;
         }
-        else
+        else if (!thrust)
         {
             FindObjectOfType<AudioManager>().Stop("rocket_thrust_sound");
+            thrustParticles.Stop();
+
+            if (boostAmount < 100f && isGrounded)
+            {
+                time += Time.time;
+                if (time > delayAmount)
+                {
+                    boostAmount = boostAmount + rechargeInterval;
+                    time = 0f;
+                }
+            }
         }
+    }
+
+    IEnumerator rechargeDelay()
+    {
+        yield return new WaitForSeconds(delayAmount);
     }
 }
